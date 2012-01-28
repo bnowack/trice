a21a6bc2f20472db0ffd754df14d61f02c6f8bfe
Benjamin Nowack <mail@bnowack.de>
2012-01-28 13:07:21 +0100

    Clean-ups in the main request catcher.

--- index.php
+++ index.php
@@ -2,28 +2,28 @@
 /**
  * Trice request catcher.
  * 
- * Should be symlink'd from (or copied to) the app's root directory.
- * All requests should then be routed to the root index.php, e.g. via mod_rewrite.
- * 
+ * Should be included (e.g. via require_once('code/trice/index.php')) from the app's root "index.php".
+ * All requests should then be routed to the root "index.php", e.g. via mod_rewrite.
  */
 
-// Code include paths (relative)
-defined('TRICE_DIR') || define('TRICE_DIR', 'code/trice/');
-defined('PHWEB_DIR') || define('PHWEB_DIR', 'code/phweb/');
-
-// Configuration file (relative)
-defined('TRICE_INI_PATH') || define('TRICE_INI_PATH', 'config/trice.ini');
-
-/* Ensure time() is E_STRICT-compliant (optional, if specified in php.ini) */
+// Ensure time() is E_STRICT-compliant (optional, if specified in php.ini)
 if (function_exists('date_default_timezone_get')) {
 	date_default_timezone_set(@date_default_timezone_get());
 }
 
-/* Load PhWeb and its autoloader */
-require_once(PHWEB_DIR . 'PhWeb.php');
+// Load PhWeb and its autoloader
+$phWebDir = rtrim(realpath("code/phweb"), '/') . '/';
+require_once("{$phWebDir}PhWeb.php");
 
-/* Load Trice and its autoloader */
-require_once(TRICE_DIR . 'Trice.php');
+// Load Trice and its autoloader
+$triceDir = rtrim(realpath("code/trice"), '/') . '/';
+require_once("{$triceDir}Trice.php");
 
-/* Let Trice take it from here */
+// Define the path to the configuration file (can be relative)
+defined('TRICE_INI_PATH') || define('TRICE_INI_PATH', 'config/trice.ini');
+
+// Define the path to the code directory (can be relative)
+defined('TRICE_INCLUDE_DIR') || define('TRICE_INCLUDE_DIR', 'code/');
+
+// Let Trice take it from here
 trice\Trice::handleRequest();
